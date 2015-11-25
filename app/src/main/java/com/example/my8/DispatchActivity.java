b9bda702636deab70b03670490fa590f7401539d
Jaemyung Yu <jaemyung.u@gmail.com>
2015-11-25 22:59:54 +0900

    Basic login functionality implemented

---
+++ app/src/main/java/com/example/my8/DispatchActivity.java
@@ -0,0 +1,29 @@
+package com.example.my8;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.parse.ParseUser;
+
+/**
+ * Activity which starts an intent for either the logged in (MainActivity) or logged out
+ * (SignUpOrLoginActivity) activity.
+ */
+public class DispatchActivity extends AppCompatActivity {
+    public DispatchActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Check if there is current user info
+        if (ParseUser.getCurrentUser() != null) {
+            // Start an intent for the logged in activity
+            startActivity(new Intent(this, MainActivity.class));
+        } else {
+            // Start and intent for the logged out activity
+            startActivity(new Intent(this, WelcomeActivity.class));
+        }
+    }
+}
---
+++ app/src/main/java/com/example/my8/WelcomeActivity.java
@@ -0,0 +1,37 @@
+package com.example.my8;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        // Log in button click handler
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Starts an intent of the log in activity
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            }
+        });
+
+        // Sign up button click handler
+        Button signupButton = (Button) findViewById(R.id.signup_button);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Starts an intent for the sign up activity
+                startActivity(new Intent(WelcomeActivity.this, SignUpActivity.class));
+            }
+        });
+    }
+}
