f6fe7d71c55e5c8a07fca0289191297ab86d867a
Jaemyung Yu <jaemyung.u@gmail.com>
2015-11-29 02:58:11 +0900

    "ready for event show in playground ver 4"

--- app/src/main/java/com/example/my8/WelcomeActivity.java
+++ app/src/main/java/com/example/my8/WelcomeActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.parse.ParseImageView;
+
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
